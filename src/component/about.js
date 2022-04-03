17eca7d35bb27c18aacb9d80ad84a455bc73fc1e
amit <amit.kumar@antiersolutions.com>
2022-04-03 12:49:29 +0530

    first commit

---
+++ src/component/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+function About () {
+    return <div>
+        <p>"Antier Solutions is at the forefront of blockchain development and its real-world application.<br />
+         We bring our strength in blockchain and combine or complement it with other technologies to <br />
+         deliver customized solutions like white label cryptocurrency exchange development,<br />
+        P2P exchange development, security token development and marketing, and ICO development.</p>
+ 
+        Our official website :
+        <a href="https://www.antiersolutions.com/">
+            Click here to know about us/
+        </a>
+    </div>
+}
+export default About;
---
+++ src/component/contact.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+ 
+function Contact (){
+ return <address>
+            E 221<br />
+            Phase 8B, Industrial Area, Sector 74 <br />
+            Sahibzada Ajit Singh Nagar, Punjab 160059<br />
+        </address>
+}
+ 
+export default Contact;
